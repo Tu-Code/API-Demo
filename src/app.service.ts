95e2da4eb1b30b4adcadaa748db00a30c2a3bc21
Sarah Solarin <solarinsarah3@gmail.com>
2023-01-12 08:53:04 +0000

    Added external API connection

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@nestjs/common';
-
+import { HttpService } from '@nestjs/axios'
+import { response } from 'express';
+import { map } from 'rxjs';
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private data = {
+    sarah: {
+      twitterFollowers: 300,
+      youtubeFollowers: 1000,
+    },
+    james: {
+      twitterFollowers: 500,
+    }
+  };
+
+  constructor(private httpService: HttpService) {}
+
+  getUser(params) {
+    return this.httpService
+      .get(`http://api.github.com/users/${ params.username }`)
+      .pipe(
+        map((response) => response.data),
+        map((data) => ({
+          ...this.data[params.username],
+          githubFollowers: data.followers,
+          publicRepos: data.public_repos,
+          name: data.name,
+          location: data.location,
+          bio: data.bio
+        }))
+      )
   }
 }
